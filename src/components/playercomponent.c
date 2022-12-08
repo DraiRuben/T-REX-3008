a3360c9735c53ef75e68485840e494b6e018d1e0
NicolasTre <N.TREZEGUET@lahorde.school>
2022-12-08 09:50:18 +0100

    commit accessory IsSprint

--- src/components/playercomponent.c
+++ src/components/playercomponent.c
@@ -7,7 +7,7 @@ typedef struct
 	int player;
 	float pvx, pvy;
 	float speed;
-	
+	bool IsSprint;
 } PlayerComponent_Properties;
 
 void PlayerComponent_Terminate(void* properties)
@@ -51,13 +51,16 @@ void PlayerComponent_Update(H3Handle h3, H3Handle object, SH3Transform* transfor
 	if (H3_Input_IsKeyDown(K_Shift))
 	{
 		props->speed = 1.65;
+		props->IsSprint = true;
 	}
 	else
 	{
 		props->speed = 1;
+		props->IsSprint = false;
 	}
 
 	H3_Object_SetVelocity(object, props->pvx * props->speed, props->pvy * props->speed);
+
 
 
 		
@@ -69,10 +72,10 @@ void* PlayerComponent_CreateProperties()
 	PlayerComponent_Properties* properties = malloc(sizeof(PlayerComponent_Properties));
 	H3_ASSERT_CONSOLE(properties, "Failed to allocate properties");
 	properties->speed = 1;
+	properties->IsSprint = false;
+
 	return properties;
+
 }
 
-//H3_DEFINE_COMPONENT_PROPERTY_ACCESSORS_RW(PlayerComponent, float, Width);
-//H3_DEFINE_COMPONENT_PROPERTY_ACCESSORS_RW(PlayerComponent, float, Height);
-//H3_DEFINE_COMPONENT_PROPERTY_ACCESSORS_RW_EX(PlayerComponent, PLAYERCOMPONENT_TYPEID, float, Width);
-//H3_DEFINE_COMPONENT_PROPERTY_ACCESSORS_RW_EX(PlayerComponent, PLAYERCOMPONENT_TYPEID, float, Height);
+H3_DEFINE_COMPONENT_PROPERTY_ACCESSORS_RO_EX(PlayerComponent, PLAYERCOMPONENT_TYPEID, bool, IsSprint);
