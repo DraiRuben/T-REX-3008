2de68656a94244aab9d665850c87148654c2ae70
NicolasTre <N.TREZEGUET@lahorde.school>
2022-12-08 16:30:12 +0100

    commit better map

--- src/components/playercomponent.c
+++ src/components/playercomponent.c
@@ -5,9 +5,10 @@
 typedef struct
 {
 	int player;
-	float pvx, pvy;
 	float speed;
+	float pvx, pvy;
 	bool IsSprint;
+	
 } PlayerComponent_Properties;
 
 void PlayerComponent_Terminate(void* properties)
@@ -46,7 +47,6 @@ void PlayerComponent_Update(H3Handle h3, H3Handle object, SH3Transform* transfor
 	{
 		props->pvx = 0;
 	}
-
 	//sprint
 	if (H3_Input_IsKeyDown(K_Shift))
 	{
@@ -61,10 +61,6 @@ void PlayerComponent_Update(H3Handle h3, H3Handle object, SH3Transform* transfor
 
 	H3_Object_SetVelocity(object, props->pvx * props->speed, props->pvy * props->speed);
 
-
-
-		
-
 }
 
 void* PlayerComponent_CreateProperties()
@@ -73,9 +69,5 @@ void* PlayerComponent_CreateProperties()
 	H3_ASSERT_CONSOLE(properties, "Failed to allocate properties");
 	properties->speed = 1;
 	properties->IsSprint = false;
-
 	return properties;
-
 }
-
-H3_DEFINE_COMPONENT_PROPERTY_ACCESSORS_RO_EX(PlayerComponent, PLAYERCOMPONENT_TYPEID, bool, IsSprint);
--- src/main.c
+++ src/main.c
@@ -1,14 +1,18 @@
 #include <h3.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "components/MainMenu.h"
 #include "components/Credits.h"
 #include "components/Settings.h"
 #include "components/textcomponent.h"
 #include "components/spritecomponent.h"
 #include "components/maplayercomponent.h"
-#include "components/energycomponent.h"
+#include "components/tirednesscomponent.h"
 #include "components/mycameracomponent.h"
+#include "components/ClockComponent.h"
 #include "components/playercomponent.h"
+#include "components/EnemyComponent.h"
 #ifndef NDEBUG
 # pragma comment(lib, "h3-s-d.lib")
 #else // !NDEBUG
@@ -29,7 +33,17 @@ int main()
 			.isUnderlined = false,
 			.isViewLocal = false,
 	};
-
+	SH3TextProperties clockprops = (SH3TextProperties){
+			.font = H3_Font_Load("assets/Comfortaa-regular.ttf"),
+			.size = 12,
+			.fillColor = {.r = 255,.g = 255,.b = 255,.a = 255},
+			.hasOutline = false,
+			.anchor = 0x22,
+			.isBold = false,
+			.isItalic = false,
+			.isUnderlined = false,
+			.isViewLocal = false,
+	};
 	//Window Init
 	H3Handle screen = H3_Init((SH3InitParams) {
 			.width = 1920,
@@ -37,14 +51,11 @@ int main()
 			.fullscreen = false,
 			.windowTitle = "T-REX3008"
 	});
-
 	bool IsMainMenu = true;
 	bool IsCredits = false;
 	bool IsSettings = false;
 	bool IsNewGame = false;
-
 	while (1) {
-
 		//Main Menu
 		if (IsMainMenu) {
 			H3Handle MainMenuScene = H3_Scene_Create(screen, true);
@@ -98,15 +109,12 @@ int main()
 			H3Handle fullBar = H3_Texture_Load("assets/AllBar.png", &barWidth, &barHeight);
 			H3Handle backBar = H3_Texture_Load("assets/EmptyBar.png", &backBarWidth, &backBarHeight);
 
-			//bar of tiredness
-			H3Handle energyBar = H3_Object_Create2(GameScene, "energybar", NULL, 5);
-			H3_Object_AddComponent(energyBar, ENERGYCOMPONENT_CREATE(fullBar, backBar));
-			H3_Object_SetTranslation(energyBar, 30, 5);
+			
 
 			//temp Map init
 			H3Handle map = H3_Map_Load("assets/map.tmx");
 			H3_Map_RegisterObjectLayerForPhysicsInScene(GameScene, map, "collider");
-			H3Handle mapplayer = H3_Object_Create2(GameScene, "layer floor", NULL, 1);
+			H3Handle mapplayer = H3_Object_Create2(GameScene, "layer floor", NULL,1);
 			H3_Object_AddComponent(mapplayer, MAPLAYERCOMPONENT_CREATE(map, "floor"));
 			H3Handle mapplayer1 = H3_Object_Create2(GameScene, "layer object", NULL,4);
 			H3_Object_AddComponent(mapplayer1, MAPLAYERCOMPONENT_CREATE(map, "object"));
@@ -118,17 +126,39 @@ int main()
 			//player
 			H3Handle player = H3_Object_Create2(GameScene, "player", NULL,3);
 			H3_Object_AddComponent(player, SPRITECOMPONENT_CREATE("assets/p.png", 0x22));
-			H3_Object_AddComponent(player, PLAYERCOMPONENT_CREATE());
 			H3_Object_EnablePhysics(player, H3_BOX_COLLIDER(CDT_Dynamic, 25, 35, 0x22, false));
+			H3_Object_AddComponent(player, PLAYERCOMPONENT_CREATE());
 			H3_Object_SetTranslation(player, 960, 540);
-			
 
 			//camera 
 			H3Handle camera = H3_Object_Create(GameScene, "camera", NULL);
 			H3_Object_AddComponent(camera, MYCAMERACOMPONENT_CREATE(480, 270, player));
 
-			while (IsNewGame) 
-		    {
+			//bar of tiredness
+			H3Handle emptyBar = H3_Object_Create2(GameScene, "emptyBar", camera, 5);
+			H3_Object_AddComponent(emptyBar, SPRITECOMPONENT_CREATE("assets/EmptyBar.png", A_Left + A_Top));
+			H3_Object_SetTranslation(emptyBar, -235, -130);
+
+			H3Handle energyBar = H3_Object_Create2(GameScene, "energybar", camera, 5);
+			H3_Object_AddComponent(energyBar, TIREDNESSCOMPONENT_CREATE(fullBar));
+			H3_Object_SetTranslation(energyBar, -234, -128);
+
+			
+			//Time
+			H3Handle time = H3_Object_Create2(GameScene, "Clock", camera, 5);
+			H3_Object_AddComponent(time, CLOCKCOMPONENT_CREATE(&clockprops));
+			//raycast init
+			int raycast_index = 0;
+			//enemies init
+			char enemies[256];
+			int enemy_index = 0;
+			bool IsWave = false;
+			for (int i = 0; i < 5; i++) {
+				snprintf(enemies, 256, "enemy_%d", enemy_index++);
+				H3Handle enemy = H3_Object_Create2(GameScene, enemies, NULL, 3);
+				//H3_Object_AddComponent(enemy, ENEMYCOMPONENT_CREATE(&player, &raycast_index, &GameScene));
+			}
+			while (IsNewGame) {
 				H3_DoFrame(screen, GameScene);
 			}
 			H3_Texture_Destroy(fullBar);
