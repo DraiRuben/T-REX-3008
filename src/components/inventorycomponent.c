46ffb6b91f0d257867655037f3b84af2ba52eff4
DraiRuben <r.drai@lahorde.school>
2022-12-09 09:39:41 +0100

    day 5 initial commit

--- src/components/inventorycomponent.c
+++ src/components/inventorycomponent.c
@@ -11,7 +11,7 @@ typedef struct
 	H3Handle triggerObj;
 	H3Handle ObjSlot0;
 	H3Handle ObjSlot1;
-	H3Handle ObjSlot2;
+	H3Handle ObjSlot2;	//hand
 } InventoryComponent_Properties;
 
 void InventoryComponent_Terminate(void* properties)
@@ -22,25 +22,38 @@ void InventoryComponent_Terminate(void* properties)
 void InventoryComponent_Update(H3Handle h3, H3Handle object, SH3Transform* transform, float t, float dt, void* properties)
 {
 	InventoryComponent_Properties* props = (InventoryComponent_Properties*)properties;
-	
+
 	//get player pos
 	H3_Transform_GetPosition(H3_Object_GetTransform(object), &props->playerX, &props->playerY);
 
-	//add an object in inventory
-	if (props->triggerObj != NULL && H3_Input_IsMouseBtnPressed(MB_Left))
+	//drop an object
+	if (H3_Input_IsKeyPressed(K_Space) && props->ObjSlot2 != NULL)
 	{
-		if (props->ObjSlot0 == NULL) {
-			props->ObjSlot0 = props->triggerObj;
-			props->triggerObj = NULL;
-		}
-		else if (props->ObjSlot1 == NULL) {
-			props->ObjSlot1 = props->triggerObj;
-			props->triggerObj = NULL;
-		}
-		else if (props->ObjSlot2 == NULL) {
-			props->ObjSlot2 = props->triggerObj;
-			props->triggerObj = NULL;
-		}
+		float angle = rand() % 361;
+		H3_Object_SetTranslation(props->ObjSlot2, props->playerX, props->playerY);
+		H3_Object_Rotate(props->ObjSlot2, angle);
+		H3_Object_SetRenderOrder(props->ObjSlot2, 2);
+		props->ObjSlot2 = NULL;
+	}
+
+	//add an object in the hand
+	if (H3_Input_IsMouseBtnPressed(MB_Left) &&
+		props->triggerObj != NULL &&
+		props->ObjSlot2 == NULL)
+	{
+
+		props->ObjSlot2 = props->triggerObj;
+		props->triggerObj = NULL;
+	}
+
+	//change the object in hand with an object in inventory
+	if (H3_Input_IsMouseBtnPressed(MB_Right))
+	{
+		H3Handle temporarySlot2 = props->ObjSlot2;
+
+		props->ObjSlot2 = props->ObjSlot1;
+		props->ObjSlot1 = props->ObjSlot0;
+		props->ObjSlot0 = temporarySlot2;
 	}
 
 	//let object in the right slot
@@ -61,7 +74,7 @@ void InventoryComponent_Update(H3Handle h3, H3Handle object, SH3Transform* trans
 void InventoryComponent_OnTriggerEnter(H3Handle object, SH3Collision collision)
 {
 	SH3Component* component = H3_Object_GetComponent(object, INVENTORYCOMPONENT_TYPEID);
-	InventoryComponent_Properties* props = (InventoryComponent_Properties*) component->properties;
+	InventoryComponent_Properties* props = (InventoryComponent_Properties*)component->properties;
 
 	props->triggerObj = collision.other;
 	props->nbTrigger++;
@@ -71,7 +84,7 @@ void InventoryComponent_OnTriggerLeave(H3Handle object, H3Handle other)
 {
 	SH3Component* component = H3_Object_GetComponent(object, INVENTORYCOMPONENT_TYPEID);
 	InventoryComponent_Properties* props = (InventoryComponent_Properties*)component->properties;
-	
+
 	props->nbTrigger--;
 	if (props->nbTrigger == 0)
 		props->triggerObj = NULL;
